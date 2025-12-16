255cc7493c4ed102fedd09af41c3e529129e1cb1
OlgaStarchenko <o9915530@gmail.com>
2025-12-16 20:13:01 +1000

    The product rendering function "renderProducts" and the function for adding a product to the cart "addProductToCart" have been written. An empty array "cartArray" has been created, the logic for handling a repeating product has been built, and event listeners have been attached to the "Add to Cart" and "-" buttons.

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const cart = document.querySelector(".cart");
 const btnCloseCart = document.querySelector(".btn__close__cart");
 const products = document.querySelector(".products");
 const productTemplate = document.querySelector(".product__template");
+const cartTemplate = document.querySelector(".cart__template");
+const cartProducts = document.querySelector(".cart__products");
+let cartArray = [];
+
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    renderProducts(data.products);
+  });
 
 function renderProducts(arr) {
+  products.innerHTML = null;
   arr.forEach((el) => {
     const clone = productTemplate.content.cloneNode(true);
     const cloneImg = clone.querySelector(".product__img");
@@ -12,6 +23,48 @@ function renderProducts(arr) {
     const cloneDescription = clone.querySelector(".product__description");
     const clonePrice = clone.querySelector(".product__price");
     const cloneBtn = clone.querySelector(".product__btn");
+    cloneImg.src = el.thumbnail;
+    cloneTitle.innerText = el.title;
+    cloneDescription.innerText = el.description;
+    clonePrice.innerText = `$ ${el.price}`;
+    products.append(clone);
+    cloneBtn.addEventListener("click", () => {
+      addProductToCart(el);
+    });
+  });
+}
+
+function addProductToCart(el) {
+  const currentProduct = cartArray.find((product) => product.id === el.id);
+  if (currentProduct) {
+    currentProduct.count++;
+  } else {
+    cartArray.push({ ...el, count: 1 });
+  }
+
+  renderCart();
+}
+
+function renderCart() {
+  cartProducts.innerHTML = null;
+  cartArray.forEach((el) => {
+    const clone = cartTemplate.content.cloneNode(true);
+    const cloneImg = clone.querySelector(".photo__cart");
+    const cloneTitle = clone.querySelector(".title__cart");
+    const cloneDescription = clone.querySelector(".description__cart");
+    const clonePrice = clone.querySelector(".price");
+    const cloneBtnLess = clone.querySelector(".less");
+    const cloneQuantity = clone.querySelector(".quantity");
+    const cloneBtnMore = clone.querySelector(".more");
+    cloneImg.src = el.thumbnail;
+    cloneTitle.innerText = el.title;
+    cloneDescription.innerText = el.description;
+    clonePrice.innerText = `$ ${el.price}`;
+    cloneQuantity.innerText = el.count;
+    cloneBtnMore.addEventListener("click", () => {
+      addProductToCart(el);
+    });
+    cartProducts.append(clone);
   });
 }
 
